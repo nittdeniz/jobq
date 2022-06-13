4453f0d83b2dd352db3c6da635cff8ed6ba072e0
nittdeniz <deniz.nitt@physik.tu-darmstadt.de>
2022-06-13 15:56:55 +0200

    removed & from starting command

--- main.cpp
+++ main.cpp
@@ -101,7 +101,7 @@ void start(Job& job){
         }
         cmd << core_ids[i];
     }
-    cmd << " " << job.command << " 2> " << job.cerr << " 1> " << job.cout << "&' &> " << OUTPUT_BUFFER << " &";
+    cmd << " " << job.command << " 2> " << job.cerr << " 1> " << job.cout << "&' &> " << OUTPUT_BUFFER;
     std::cerr << "cmd: " << cmd.str() << "\n";
     std::system(cmd.str().c_str());
     std::this_thread::sleep_for(50ms);

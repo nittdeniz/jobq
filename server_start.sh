37765ba8aef211a68830c2d1bf22100c3674dbfe
nittdeniz <deniz.nitt@physik.tu-darmstadt.de>
2022-06-15 10:56:13 +0200

    fixed typo

--- server_start.sh
+++ server_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! pgrep -x jobq_server > /dev/null; then
-  /etc/jobq/job_server &> /dev/null 2>&1 &
+  /etc/jobq/jobq_server &> /dev/null 2>&1 &
   if pgrep -x jobq_server > /dev/null; then
     echo "Server started."
   else

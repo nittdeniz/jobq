c20f67f0fdba43fbb5728216cfce1ecfc32ea68b
nittdeniz <deniz.nitt@physik.tu-darmstadt.de>
2022-06-15 11:02:05 +0200

    added debug output to server_stop

--- source/jobq_server_stop.cpp
+++ source/jobq_server_stop.cpp
@@ -3,12 +3,15 @@
 
 #include <chrono>
 #include <fstream>
+#include <iostream>
 #include <thread>
 
 int main(){
 	using namespace std::chrono_literals;
+	std::cerr << "server stop\n";
 	while( JobQ::is_locked(COMMAND_LOCK_FILE) ){
 		std::this_thread::sleep_for(50ms);
+		std::cerr << "locked\n";
 	}
 	JobQ::lock_file(COMMAND_LOCK_FILE);
 	std::ofstream out(COMMAND_FILE);

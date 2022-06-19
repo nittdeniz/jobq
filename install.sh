487ce86d05e70599838cfcb17de5185e60f12df5
nittdeniz <deniz.nitt@physik.tu-darmstadt.de>
2022-06-19 12:35:33 +0200

    changed jobq_server_start name

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ sudo mkdir -p /etc/jobq
 sudo setfacl -m u:$USER:rw /etc/jobq
 sudo chown -R $USER /etc/jobq
 cp jobq_exec.sh /etc/jobq/jobq_exec.sh
+sudo cp jobq_server_start.sh /usr/local/sbin/jobq_server_start
 mkdir -p build
 cd build
 make
 sudo cp jobq_server /usr/local/sbin/jobq_server
-sudo cp jobq_server_start /usr/local/sbin/jobq_server_start
 sudo cp jobq_server_stop /usr/local/sbin/jobq_server_stop
 sudo cp jobq_status /usr/local/sbin/jobq_status
 sudo cp jobq_stop /usr/local/sbin/jobq_stop

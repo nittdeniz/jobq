563e25c327a8b7ac670194b8b2a7aaf0796366ee
nittdeniz <deniz.nitt@physik.tu-darmstadt.de>
2022-06-19 12:32:14 +0200

    added sudo to copy

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+git pull
 sudo mkdir -p /etc/jobq
 sudo setfacl -m u:$USER:rw /etc/jobq
 sudo chown -R $USER /etc/jobq
 cp jobq_exec.sh /etc/jobq/jobq_exec.sh
 mkdir -p build
 cd build
-git pull
 make
-cp jobq_server /usr/local/sbin/jobq_server
-cp jobq_server_start /usr/local/sbin/jobq_server_start
-cp jobq_server_stop /usr/local/sbin/jobq_server_stop
-cp jobq_status /usr/local/sbin/jobq_status
-cp jobq_stop /usr/local/sbin/jobq_stop
-cp jobq_submit /usr/local/sbin/jobq_submit
+sudo cp jobq_server /usr/local/sbin/jobq_server
+sudo cp jobq_server_start /usr/local/sbin/jobq_server_start
+sudo cp jobq_server_stop /usr/local/sbin/jobq_server_stop
+sudo cp jobq_status /usr/local/sbin/jobq_status
+sudo cp jobq_stop /usr/local/sbin/jobq_stop
+sudo cp jobq_submit /usr/local/sbin/jobq_submit

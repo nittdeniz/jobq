a1dc5a13cd3dc51f0bc10b651903fff4e9f4861a
nittdeniz <deniz.nitt@physik.tu-darmstadt.de>
2022-06-22 06:28:32 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 sudo mkdir -p /etc/jobq
-sudo setfacl -m u:$USER:rxw /etc/jobq
-sudo chown -R $USER /etc/jobq
-cp jobq_exec.sh /etc/jobq/jobq_exec.sh
-sudo cp jobq_server_start.sh /usr/local/sbin/jobq_server_start
+sudo chown -R root:jobq /etc/jobq
+
 mkdir -p build
 cd build
 make
-sudo cp jobq_server /usr/local/sbin/jobq_server
-sudo cp jobq_server_stop /usr/local/sbin/jobq_server_stop
-sudo cp jobq_status /usr/local/sbin/jobq_status
-sudo cp jobq_stop /usr/local/sbin/jobq_stop
-sudo cp jobq_submit /usr/local/sbin/jobq_submit
+cd ..
+
+sudo cp jobq_exec.sh /etc/jobq/.jobq_exec
+sudo cp jobq_server_start.sh /usr/local/sbin/jobq_server_start
+sudo cp build/jobq_server /usr/local/sbin/jobq_server
+sudo cp build/jobq_server_stop /usr/local/sbin/jobq_server_stop
+sudo cp build/jobq_status /usr/local/sbin/jobq_status
+sudo cp build/jobq_stop /usr/local/sbin/jobq_stop
+sudo cp build/jobq_submit /usr/local/sbin/jobq_submit
+
+sudo chown -R root:jobq  /usr/local/sbin/jobq_server
+sudo chown -R root:jobq  /usr/local/sbin/jobq_server_stop
+sudo chown -R root:jobq  /usr/local/sbin/jobq_status
+sudo chown -R root:jobq  /usr/local/sbin/jobq_stop
+sudo chown -R root:jobq  /usr/local/sbin/jobq_submit
+
+sudo setfacl -Rm g:jobq:rxw /etc/jobq
+sudo setfacl -Rm g:jobq:rxw /usr/local/sbin
+sudo setfacl -Rx u:jobq /etc/jobq
+sudo setfacl -Rx u:jobq /usr/local/sbin
+
+sudo setfacl -m g:jobq:x /usr/local/sbin/jobq_submit
+sudo setfacl -m g:jobq:x /usr/local/sbin/jobq_server
+sudo setfacl -m g:jobq:x /usr/local/sbin/jobq_server_stop
+sudo setfacl -m g:jobq:x /usr/local/sbin/jobq_server_start
+sudo setfacl -m g:jobq:x /usr/local/sbin/jobq_status
+sudo setfacl -m g:jobq:x /usr/local/sbin/jobq_stop
+
+sudo setfacl -x g:jobq /usr/local/sbin/jobq_submit
+sudo setfacl -x g:jobq /usr/local/sbin/jobq_server
+sudo setfacl -x g:jobq /usr/local/sbin/jobq_server_stop
+sudo setfacl -x g:jobq /usr/local/sbin/jobq_server_start
+sudo setfacl -x g:jobq /usr/local/sbin/jobq_status
+sudo setfacl -x g:jobq /usr/local/sbin/jobq_stop
+sudo setfacl -x g:jobq /usr/local/sbin/jobq_submit

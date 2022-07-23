9ceeb8e98e98032bc2b6d9f43b957aba4f3da633
nittdeniz <deniz.nitt@physik.tu-darmstadt.de>
2022-07-23 07:39:17 +0200

    new version in C with sockets

--- source/commands.cpp
+++
@@ -1,7 +0,0 @@
-#include "commands.hpp"
-
-namespace JobQ{
-    std::string const CMD_SERVER_STOP = "stopserver";
-    std::string const CMD_SERVER_STATUS = "status";
-    std::string const CMD_STOP  = "stop";
-}
--- source/job.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by deniz on 13.06.2022.
-//
-
-#include "job.hpp"
